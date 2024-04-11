a7cea3e32c9e405bb1f2a1ee62a881f074bcca96
MarcosVini9999 <marcosviniciusandradedesousa@Hotmail.com>
2024-04-11 08:15:30 -0300

    feat: day 02

--- backend/src/infra/socket/webSocket.ts
+++ backend/src/infra/socket/webSocket.ts
@@ -1,9 +1,62 @@
 import { io } from "../../app";
 
-io.on("connection", (socket) => {
-  console.log("usuário conectado");
+interface User {
+  socket_id: string;
+  username: string;
+  room: string;
+}
 
-  socket.on("select-room", (data) => {
-    console.log(data);
+interface Message {
+  text: string;
+  room: string;
+  create_at: Date;
+  userName: string;
+}
+
+const users: Array<User> = [];
+const messages: Array<Message> = [];
+
+io.on("connection", (socket) => {
+  socket.on("select_room", (data, callback) => {
+    console.log("esse código foi executado", data, callback);
+    socket.join(data.room);
+
+    const userIsInRoom = users.find(
+      (user) => user.username === data.username && user.room === data.room
+    );
+
+    if (userIsInRoom) {
+      userIsInRoom.socket_id = socket.id;
+    } else {
+      users.push({
+        room: data.room,
+        username: data.username,
+        socket_id: socket.id,
+      });
+    }
+
+    io.to(data.room).emit("useronline", users);
+
+    const dataResponse = {
+      messageRoom: messages.filter((message) => message.room === data.room),
+      userRoom: users.filter((user) => user.room === data.room),
+    };
+
+    callback(dataResponse);
+  });
+  socket.on("message", (data) => {
+    // Criando a mensagem
+    const message: Message = {
+      text: data.message,
+      room: data.room,
+      create_at: new Date(),
+      userName: data.userName,
+    };
+
+    // Adicionando a mensagem ao array de mensagens
+    messages.push(message);
+
+    // Emitindo a mensagem para todos os usuários da sala
+    io.to(data.room).emit("message", message);
   });
 });
--- frontend/chat.js
+++ frontend/chat.js
@@ -5,6 +5,36 @@ const urlParams = new URLSearchParams(window.location.search);
 const username = urlParams.get("username");
 const room = urlParams.get("select-row");
 
-socket.emit("select-room", { username, room });
+const createMessage = (data) => {
+  const MessageDiv = document.getElementById("message-container");
 
-socket.emit("hello", { name: "Jhonatas" });
+  if (username === data.username) {
+    MessageDiv.innerHTML += `<div class="user-message message">
+    <span class="name">${data.username}</span>
+    <p>${data.text}</p>
+    <span class="data">${dayjs(data.create_at).format("DD/MM HH:mm")}</span>
+    </div>`;
+  } else {
+    MessageDiv.innerHTML += `<div class="other-message message">
+    <span class="name">${data.username}</span>
+    <p>${data.text}</p>
+    <span class="data">${dayjs(data.create_at).format("DD/MM HH:mm")}</span>
+    </div>`;
+  }
+};
+
+const createUsersOnline = (data) => {
+  const ulUser = document.getElementById("online");
+  ulUser.innerHTML = "";
+
+  data.forEach((user) => {
+    if (user.username != null) {
+      ulUser.innerHTML += `<li><a href="#">${user.username}</a></li>`;
+    }
+  });
+};
+
+socket.emit("select_room", { username, room }, (dataResponse) => {
+  dataResponse.messageRoom.forEach((message) => createMessage(message));
+  createUsersOnline(dataResponse.userRoom);
+});
